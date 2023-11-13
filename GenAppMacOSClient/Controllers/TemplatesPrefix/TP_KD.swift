c7f7b72cf98aa764cbbd8b16a385ad3923433e93
Yuri Yunitski <mvoznicki@gmail.com>
2023-11-13 18:17:53 +0300

    kd quiz ref

--- GenAppMacOSClient/Controllers/TemplatesPrefix/TP_KD.swift
+++ GenAppMacOSClient/Controllers/TemplatesPrefix/TP_KD.swift
@@ -133,16 +133,16 @@ extension GenAppController {
                 )
             ]
             
-//        case .kdComposeQuiz:
-//            return [
-//                KDComposeQuizTemplate.shared.template1(
-//                    backColorPrimary: .init(hex: values.ui?.backColorPrimary) ?? .accentColor,
-//                    primaryColor: .init(hex: values.ui?.primaryColor) ?? .accentColor,
-//                    backColorSecondary: .init(hex: values.ui?.backColorSecondary) ?? .accentColor,
-//                    textColorPrimary: .init(hex: values.ui?.textColorPrimary) ?? .accentColor,
-//                    buttonColorPrimary: .init(hex: values.ui?.buttonColorPrimary) ?? .accentColor
-//                )
-//            ]
+        case .kdComposeQuiz:
+            return [
+                KDComposeQuizTemplate.shared.template1(
+                    backColorPrimary: .init(hex: values.ui?.backColorPrimary) ?? .accentColor,
+                    primaryColor: .init(hex: values.ui?.primaryColor) ?? .accentColor,
+                    backColorSecondary: .init(hex: values.ui?.backColorSecondary) ?? .accentColor,
+                    textColorPrimary: .init(hex: values.ui?.textColorPrimary) ?? .accentColor,
+                    buttonColorPrimary: .init(hex: values.ui?.buttonColorPrimary) ?? .accentColor
+                )
+            ]
         default: return []
         }
     }
